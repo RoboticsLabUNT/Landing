ba0862d8fcd74ae82fd66d3dd61330f6cfff1d26
elmichicho <wparedes@unitru.edu.pe>
2024-07-11 01:28:47 -0500

    carrusel automatico

---
+++ scripts/slider_auto.js
@@ -0,0 +1,24 @@
+// Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtenemos todos los inputs de tipo radio dentro del carrusel
+    const slides = document.querySelectorAll('.slider input[type="radio"]');
+    let currentIndex = 0; // Índice inicial del slide actual
+
+    // Función para avanzar al siguiente slide
+    function nextSlide() {
+        // Desmarca el input actual
+        slides[currentIndex].checked = false;
+        // Avanza al siguiente slide
+        currentIndex = (currentIndex + 1) % slides.length;
+        // Marca el siguiente input como checked
+        slides[currentIndex].checked = true;
+    }
+
+    // Función para iniciar el carrusel automático
+    function startCarousel() {
+        setInterval(nextSlide, 5000); // Cambia cada 2 segundos (2000 milisegundos)
+    }
+
+    // Inicia el carrusel automático cuando se carga completamente el DOM
+    startCarousel();
+});
