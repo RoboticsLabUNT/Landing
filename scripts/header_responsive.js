e9329f71dac2f8b2dc98eb5d41db54fdd1b98d3e
elmichicho <wparedes@unitru.edu.pe>
2024-07-14 23:38:07 -0500

    header resposnive correcion

--- scripts/header_responsive.js
+++ scripts/header_responsive.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const sideMenu = document.querySelector('.side-menu');
     const closeBtn = document.querySelector('.side-menu .close-btn');
+    const dropdownToggle = document.querySelector('.side-menu .dropdown-toggle');
 
     menuToggle.addEventListener('click', function() {
         sideMenu.style.display = 'block';
@@ -9,5 +10,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
     closeBtn.addEventListener('click', function() {
         sideMenu.style.display = 'none';
+    });
+
+    // Toggle dropdown when clicking on .dropdown-toggle
+    dropdownToggle.addEventListener('click', function(event) {
+        event.preventDefault();
+        const dropdownContent = this.nextElementSibling; // .dropdown-content
+        const dropdown = this.parentElement; // .dropdown
+
+        dropdown.classList.toggle('active');
+        if (dropdown.classList.contains('active')) {
+            dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px';
+        } else {
+            dropdownContent.style.maxHeight = '0';
+        }
     });
 });
