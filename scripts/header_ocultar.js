e42adbe9d8485f96431be003bd6f16aeb0571ee6
elmichicho <wparedes@unitru.edu.pe>
2024-07-14 23:49:23 -0500

    correcion header

--- scripts/header_ocultar.js
+++ scripts/header_ocultar.js
@@ -4,10 +4,13 @@ const hideDistance = 100; // Distancia en píxeles después de la cual el encabe
 
 // Función para ocultar el encabezado al hacer scroll
 function handleScroll() {
-    if (window.scrollY > hideDistance) {
-        header.style.opacity = '0'; // Oculta el encabezado
-    } else {
-        header.style.opacity = '1'; // Muestra el encabezado
+    // Verifica el ancho de la ventana
+    if (window.innerWidth > 950) {
+        if (window.scrollY > hideDistance) {
+            header.style.opacity = '0'; // Oculta el encabezado
+        } else {
+            header.style.opacity = '1'; // Muestra el encabezado
+        }
     }
 }
 
@@ -16,14 +19,20 @@ window.addEventListener('scroll', handleScroll);
 
 // Restablece el encabezado al pasar el cursor sobre él
 header.addEventListener('mouseenter', function() {
-    if (window.scrollY > hideDistance) {
-        header.style.opacity = '1'; // Muestra el encabezado
+    // Verifica el ancho de la ventana
+    if (window.innerWidth > 950) {
+        if (window.scrollY > hideDistance) {
+            header.style.opacity = '1'; // Muestra el encabezado
+        }
     }
 });
 
 // Vuelve a ocultar el encabezado cuando se retira el cursor de él
 header.addEventListener('mouseleave', function() {
-    if (window.scrollY > hideDistance) {
-        header.style.opacity = '0'; // Oculta el encabezado
+    // Verifica el ancho de la ventana
+    if (window.innerWidth > 950) {
+        if (window.scrollY > hideDistance) {
+            header.style.opacity = '0'; // Oculta el encabezado
+        }
     }
 });
